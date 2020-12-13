14c712cd3e787f67a3eacfd0878e56a7c200ac06
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-12-13 10:27:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+/**
+ * 1) In the "Variables" section, click "Make a Variable." Name it scoreCounter. 
+ * 
+ * 2) When the microbit starts, we want the scoreCounter variable to begin at 0. Find a blue basic block and red variable block to put these together!
+ * 
+ * 3) When we press a button, we want the scoreCounter variable to change by going up by 1.
+ * 
+ * 4) Also when we press the button, we need to display the score variable. Find in "Basic" the block "show number" and drag it into your button block.
+ * 
+ * 4) Go to the "Variable" section and grab the oval scoreCounter block. Replace the "0" in the "show number" block with this variable.
+ * 
+ * 5) Test and submit!
+ */
